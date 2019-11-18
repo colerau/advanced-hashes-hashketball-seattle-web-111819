211974c39e2f4b0665dd2e16117505fc9f0f72a3
Cole Rau <colevrau@gmail.com>
2019-11-18 13:07:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -108,6 +108,9 @@ def num_points_scored(name)
         data.each do |player|
           if player[:player_name] == name
             return player[:points]
+          end 
+        end 
+      end 
     end 
   end 
 end 
