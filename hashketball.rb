820608e95b5fdb12750617fceee46f2114e6699f
Cole Rau <colevrau@gmail.com>
2019-11-18 12:52:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,18 +5,18 @@ def game_hash
       colors: ["Black", "White"]
       players: [
           {
-            player_name: 
-            number: 
-            shoe: 
-            points: 
-            rebounds: 
-            assissts: 
-            steals: 
-            blocks: 
-            slam_dunks: 
+            player_name: "Alan Anderson"
+            number: 0
+            shoe: 16
+            points: 22
+            rebounds: 12
+            assists: 12
+            steals: 3
+            blocks: 1
+            slam_dunks: 1
           },
           {
-            player_name: 
+            player_name: "Reggie Evans"
             number: 
             shoe: 
             points: 
